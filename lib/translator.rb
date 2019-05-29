cda6130474c527897a1d287ad8334a9bba075a50
Alexander Schelchere <ascellie@gmail.com>
2019-05-29 07:21:25 -0400

    load_library - complete

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,27 @@
-# require modules here
+require "pry"
+require "yaml"
 
-def load_library
-  # code goes here
+
+def load_library(lib)
+  # create dictionary
+  emote_dictionary = {
+    "get_emoticon" => {}, 
+    "get_meaning" => {}
+  }
+  # binding.pry
+  # load the file
+  data = YAML.load_file(lib)
+  
+  # go through our data
+  data.map do |meaning, emotes|
+    # binding.pry  
+    # english emote to japanese emote
+    emote_dictionary["get_emoticon"][emotes[0]] = emotes[1] 
+    # japanese emote to meaning
+    emote_dictionary["get_meaning"][emotes[1]] = meaning
+  end
+
+  emote_dictionary
 end
 
 def get_japanese_emoticon
